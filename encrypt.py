4e57de473d9184b01e74dc75e5728f7a3e181001
ixink <167510114+ixink@users.noreply.github.com>
2024-04-19 14:44:12 +0600

    Create encrypt.py
    This can encrypt or decrypt any text message. 
    
    
    Setup: 
    git clone https://github.com/ixink/ixiECDC.git
    ls
    chmod +x encrypt.py
    
    Run:
    ./encrypt.py
    
    [Note: This version doesn't support any number or special character. Next time, I will include all. ]
    
    You can share encrypted message with other. If he/she have your encrypted key, only he/she can read your message.
    
    This can help you to share private message or secret message.
    
    Thanks.

---
+++ encrypt.py
@@ -0,0 +1,40 @@
+!/usr/bin/python3
+print(" _      _   _____ ____ ____  ____   ____") 
+print("(_)_  _(_) | ____/ ___/ /\ \|  _ \ / ___|")
+print("| \ \/ / | |  _|| |  / /  \ \ | | | |    ")
+print("| |>  <| | | |__| |__\ \  / / |_| | |___ ")
+print("|_/_/\_\_| |_____\____\_\/_/|____/ \____|")
+
+message=input("What would you like to do?\n1. Encrypt\n2. Decrypt\n")
+def Encryptmessage(str,key1):
+    encstring=""
+    for i in str:
+        if(ord(i))>=65 and (ord(i)<=90):
+            tempstr=(ord(i)+key1)
+            if tempstr>90:
+                tempstr=tempstr%90+64
+            encstring=encstring+chr(tempstr)
+        elif(ord(i))>97 and (ord(i)<=122):
+            tempstr=(ord(i)+key1)
+            if tempstr>122:
+                tempstr=tempstr%122+96
+            encstring=encstring+chr(tempstr)
+        else:
+            encstring=encstring+chr(ord(i)+key1)
+    return encstring
+if message=="1":
+    print("Enter the string to Encrypt")
+    str=input()
+    print("Enter the key(Eg. 21):")
+    keyp=int(input())
+    keyp=keyp%26
+    print("Encrypted string:", Encryptmessage(str, keyp))
+elif message=="2":
+    print("Enter the string to Decrypt")
+    str=input()
+    print("Enter the key(Eg. 21):")
+    keyp=-int(input())
+    keyp=keyp%26
+    print("Decrypted String: ",Encryptmessage(str, keyp))
+
+#ixi inkhat
